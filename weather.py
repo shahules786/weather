b2e07609ed7675f6bace822edac052a5cd316a94
donalexvalluvassery <donalexvalluvassery@gmail.com>
2017-10-25 17:43:53 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,26 +1,27 @@
-
 # coding: utf-8
 
 # In[2]:
 
-#weather
-import requests
-from bs4 import BeautifulSoup
-import re
-from termcolor import colored
+#weather finder made by shahul es
+try: 
+   import requests
+   from bs4 import BeautifulSoup
+   import re
+   from termcolor import colored
+except ImportError:
+   print("import error has occured")
 
-res=requests.get("https://www.msn.com/en-us/weather")
+try:
+   res=requests.get("https://www.msn.com/en-us/weather")
+except Exception as e:
+   print("No internet connection.please connect and try")
+   exit(0)
 
-bsobj=BeautifulSoup(res.text,"lxml")
+bsobj=BeautifulSoup(res.text,"html.parser")
 weather=bsobj.find("span",{"class":"current"})
 acc=bsobj.find("a",{"href":re.compile(r'(day=1)$')})
 print("\n\t\t")
-print colored(weather.attrs["aria-label"],"blue")
+print (colored(weather.attrs["aria-label"],"blue"))
 print("\n")
-print colored(acc.attrs["aria-label"],"blue")
-
-
-# In[ ]:
-
-
-
+print (colored(acc.attrs["aria-label"],"blue"))
+print(colored("\n\t\t\tTHANK YOU","red"))
